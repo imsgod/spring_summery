8e2e9044e2835b12b30d10473bc21f77df163dbc
iks <imsgod@naver.com>
2019-02-27 07:22:11 +0900

    google chart db_data connection

--- spring02/src/main/java/com/example/spring02/controller/chart/GoogleChartController.java
+++ spring02/src/main/java/com/example/spring02/controller/chart/GoogleChartController.java
@@ -1,5 +1,8 @@
 package com.example.spring02.controller.chart;
 
+import javax.inject.Inject;
+
+import org.json.simple.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,11 +11,15 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.example.spring02.model.member.dto.MemberDTO;
 import com.example.spring02.model.shop.dto.ProductDTO;
+import com.example.spring02.service.chart.GoogleChartService;
 
 //@RestController
 @Controller
 @RequestMapping("chart/*")
 public class GoogleChartController {
+	
+	@Inject
+	GoogleChartService googleChartService;
 	
 	@ResponseBody
 	@RequestMapping("chart1.do")
@@ -23,7 +30,14 @@ public class GoogleChartController {
 	@ResponseBody
 	@RequestMapping("chart2.do")
 	public ModelAndView chart2() {
-		return null;
+		return new ModelAndView("chart/chart02");
+	}
+	
+	// view(chart2)로 넘어가지 않고 호출한 곳에 jsonobject 객체를 넘겨줌(리턴)
+	@ResponseBody
+	@RequestMapping("cart_money_list.do")
+	public JSONObject cart_money_list() {
+		return googleChartService.getChartData();
 	}
 	
 	/*
---
+++ spring02/src/main/java/com/example/spring02/service/chart/GoogleChartServiceImpl.java
@@ -0,0 +1,62 @@
+package com.example.spring02.service.chart;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.springframework.stereotype.Service;
+
+import com.example.spring02.model.shop.dto.CartDTO;
+import com.example.spring02.service.shop.CartService;
+ 
+ 
+
+@Service
+public class GoogleChartServiceImpl implements GoogleChartService {
+	
+	@Inject
+	CartService cartService;      
+	
+	@Override
+	public JSONObject getChartData() {
+		//ArrayList -> json 객체로 
+		List<CartDTO> items = cartService.cartMoney();
+		//리턴할 최종 json 객체
+		JSONObject data= new JSONObject();
+		//column을 정의할 json 객체
+		JSONObject col1 = new JSONObject();
+		JSONObject col2 = new JSONObject();
+		JSONArray title = new JSONArray();
+		
+		col1.put("label", "상품명");
+		col1.put("type", "string");
+		col2.put("label", "금액");
+		col2.put("type", "number");
+		//json 배열에 json 객체 추가
+		title.add(col1);
+		title.add(col2);
+		data.put("cols", title);
+		
+		//row를 정의할  json 객체
+		JSONArray body = new JSONArray();
+		for(CartDTO dto : items) {
+			JSONObject name = new JSONObject();
+			name.put("v", dto.getProduct_name());
+			JSONObject money = new JSONObject();
+			money.put("v", dto.getMoney());
+			JSONArray row = new JSONArray();
+			row.add(name);
+			row.add(money);
+			JSONObject cel1 = new JSONObject();
+			cel1.put("c", row);
+			body.add(cel1);
+		}
+		
+		data.put("cols", title);
+		
+		return data;
+	}
+
+}
